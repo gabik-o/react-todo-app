40ab19604c39c5c93312490280cfeab5248c2a02
Gabriel Okoye <gabrielokoye@Gabriels-MacBook-Pro.local>
2022-11-01 01:17:40 -0400

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://leading-coral-26.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
